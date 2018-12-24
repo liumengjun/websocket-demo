d6b35d622b9faf7ebfc691523301df63a4435db9
Mengjun Liu <mengjun_liu@126.com>
2018-12-24 20:02:44 +0800

    Use TaskScheduler in SockJsSession

--- src/main/java/com/example/wsdemo/config/MyHandler.java
+++ src/main/java/com/example/wsdemo/config/MyHandler.java
@@ -1,11 +1,20 @@
 package com.example.wsdemo.config;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.ScheduledFuture;
+
 import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.sockjs.transport.SockJsServiceConfig;
+import org.springframework.web.socket.sockjs.transport.session.AbstractSockJsSession;
 
 public class MyHandler extends TextWebSocketHandler implements EnvironmentAware {
 
@@ -19,6 +28,13 @@ public class MyHandler extends TextWebSocketHandler implements EnvironmentAware 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         System.out.println("Session[" + session.getId() + "] is open...");
+        if (session instanceof AbstractSockJsSession) {
+            SockJsServiceConfig cfg = ((AbstractSockJsSession) session).getSockJsServiceConfig();
+            TaskScheduler taskScheduler = cfg.getTaskScheduler();
+            NTimesHello task = new NTimesHello(session);
+            ScheduledFuture future = taskScheduler.scheduleAtFixedRate(task, Duration.ofSeconds(1));
+            task.setFuture(future);
+        }
     }
 
     @Override
@@ -29,5 +45,39 @@ public class MyHandler extends TextWebSocketHandler implements EnvironmentAware 
     @Override
     public void setEnvironment(final Environment environment) {
         this.environment = environment;
+    }
+
+    public static class NTimesHello implements Runnable {
+
+        static final int MAX_TIMES = 5;
+
+        private int count;
+
+        private WebSocketSession session;
+
+        private ScheduledFuture future;
+
+        public NTimesHello(WebSocketSession session) {
+            this.session = session;
+        }
+
+        public void setFuture(ScheduledFuture future) {
+            this.future = future;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("NTimesHello: " + count);
+            if (count >= MAX_TIMES || session == null || !session.isOpen()) {
+                future.cancel(false); // 自取消
+                return;
+            }
+            try {
+                session.sendMessage(new TextMessage(new Date().toString() + " rand " + new Random().nextInt(100)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            count++;
+        }
     }
 }
