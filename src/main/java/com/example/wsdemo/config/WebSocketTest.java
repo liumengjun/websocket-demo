d685c396ac09d5e862f90eb2ddadae8c9626b4f6
Mengjun Liu <mengjun_liu@126.com>
2018-12-24 14:31:46 +0800

    Fix inject spring Environment onto @ServerEndpoint class

--- src/main/java/com/example/wsdemo/config/WebSocketTest.java
+++ src/main/java/com/example/wsdemo/config/WebSocketTest.java
@@ -29,19 +29,23 @@ public class WebSocketTest implements EnvironmentAware {
     //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。若要实现服务端与单一客户端通信的话，可以使用Map来存放，其中Key可以为用户标识
     private static CopyOnWriteArraySet<WebSocketTest> webSocketSet = new CopyOnWriteArraySet<WebSocketTest>();
 
+    private static Environment globalEnvironment;
+
     //与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
 
     @Autowired
-    private Environment environment; // FIXME: cannot inject env in spring boot
+    private Environment environment;
 
     /**
      * 连接建立成功调用的方法
-     * @param session  可选的参数。session为与某个客户端的连接会话，需要通过它来给客户端发送数据
+     *
+     * @param session 可选的参数。session为与某个客户端的连接会话，需要通过它来给客户端发送数据
      */
     @OnOpen
-    public void onOpen(Session session){
+    public void onOpen(Session session) {
         this.session = session;
+        this.environment = globalEnvironment;
         webSocketSet.add(this);     //加入set中
         addOnlineCount();           //在线数加1
         System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
@@ -51,7 +55,7 @@ public class WebSocketTest implements EnvironmentAware {
      * 连接关闭调用的方法
      */
     @OnClose
-    public void onClose(){
+    public void onClose() {
         webSocketSet.remove(this);  //从set中删除
         subOnlineCount();           //在线数减1
         System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
@@ -59,6 +63,7 @@ public class WebSocketTest implements EnvironmentAware {
 
     /**
      * 收到客户端消息后调用的方法
+     *
      * @param message 客户端发送过来的消息
      * @param session 可选的参数
      */
@@ -66,7 +71,7 @@ public class WebSocketTest implements EnvironmentAware {
     public void onMessage(String message, Session session) {
         System.out.println("来自客户端的消息:" + message);
         //群发消息
-        for(WebSocketTest item: webSocketSet){
+        for (WebSocketTest item : webSocketSet) {
             try {
                 item.sendMessage(message);
             } catch (IOException e) {
@@ -78,21 +83,17 @@ public class WebSocketTest implements EnvironmentAware {
 
     /**
      * 发生错误时调用
-     * @param session
-     * @param error
      */
     @OnError
-    public void onError(Session session, Throwable error){
+    public void onError(Session session, Throwable error) {
         System.out.println("发生错误");
         error.printStackTrace();
     }
 
     /**
      * 这个方法与上面几个方法不一样。没有用注解，是根据自己需要添加的方法。
-     * @param message
-     * @throws IOException
      */
-    public void sendMessage(String message) throws IOException{
+    public void sendMessage(String message) throws IOException {
         this.session.getBasicRemote().sendText(message);
         //this.session.getAsyncRemote().sendText(message);
     }
@@ -112,5 +113,8 @@ public class WebSocketTest implements EnvironmentAware {
     @Override
     public void setEnvironment(final Environment environment) {
         this.environment = environment;
+        if (globalEnvironment == null && environment != null) {
+            globalEnvironment = environment;
+        }
     }
 }
